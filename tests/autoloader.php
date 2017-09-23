811dd5ffde351650a56288c9af67d73c52415b49
Richard Klees <richard.klees@rwth-aachen.de>
2017-09-23 18:21:48 +0200

    added autoloader for tests.

---
+++ tests/autoloader.php
@@ -0,0 +1,10 @@
+<?php
+/******************************************************************************
+ * DESCRIPTION
+ * Copyright (c) 2014, 2015 Richard Klees <richard.klees@rwth-aachen.de>
+ *
+ * This software is licensed under The MIT License. You should have received 
+ * a copy of the along with the code.
+ */
+
+require_once(__DIR__."/../vendor/autoload.php");
