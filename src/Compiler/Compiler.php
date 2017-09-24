39e566730a8ffb0cfe6b5972912b473070099b11
Richard Klees <richard.klees@rwth-aachen.de>
2017-09-24 14:31:53 +0200

    added some basic classes for the compiler

---
+++ src/Compiler/Compiler.php
@@ -0,0 +1,37 @@
+<?php
+/**********************************************************************
+ * php.js runs PHP code on the client side using javascript.
+ * Copyright (C) 2017 Richard Klees <richard.klees@rwth-aachen.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ **********************************************************************/
+
+namespace Lechimp\PHP_JS\Compiler;
+
+use Lechimp\PHP_JS\JS;
+use PhpParser\Node;
+
+/**
+ * Compile PHP to JS.
+ */
+class Compiler {
+    /**
+     * Compile PHP to JS.
+     *
+     * @param   Node\Stmt[] $stmts
+     * @return  JS\Node[]
+     */
+    public function toJS($indentation) {
+    }
+}
