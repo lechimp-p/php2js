01ac688922b7a945bc8b4100d251dc5eaed1cd06
Richard Klees <richard.klees@rwth-aachen.de>
2019-03-03 20:59:42 +0100

    added dependency locator based on psr4

---
+++ src/Compiler/Dependency/Psr4Locator.php
@@ -0,0 +1,64 @@
+<?php
+/**********************************************************************
+ * php.js runs PHP code on the client side using javascript.
+ * Copyright (C) 2017 Richard Klees <richard.klees@rwth-aachen.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ **********************************************************************/
+
+declare(strict_types=1);
+
+namespace Lechimp\PHP_JS\Compiler\Dependency;
+
+use Composer\Autoload\ClassLoader;
+
+class Psr4Locator implements Locator {
+    /**
+     * @var Locator
+     */
+    protected $inner;
+
+    /**
+     * @var ClassLoader
+     */
+    protected $class_loader;
+
+    public function __construct(Locator $inner, string $namespace, string $path) {
+        $this->inner = $inner;
+        $this->class_loader = new ClassLoader();
+        $this->class_loader->addPsr4($namespace, $path);
+    }
+
+    /**
+     * @inheritdocs
+     */
+    public function isInternalDependency(string $name) : bool {
+        if ($this->class_loader->findFile($name)) {
+            return false;
+        }
+        return $this->inner->isInternalDependency($name);
+    }
+
+    /**
+     * @inheritdocs
+     */
+    public function getFilenameOfDependency(string $name) {
+        $filename = $this->class_loader->findFile($name);
+        if ($filename) {
+            return $filename;
+        }
+        return $this->inner->getFilenameOfDependency($name);
+    }
+}
+
---
+++ tests/Compiler/Dependency/Psr4LocatorTest.php
@@ -0,0 +1,80 @@
+<?php
+/**********************************************************************
+ * php.js runs PHP code on the client side using javascript.
+ * Copyright (C) 2017 Richard Klees <richard.klees@rwth-aachen.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ **********************************************************************/
+
+declare(strict_types=1);
+
+namespace Lechimp\PHP_JS\Test\Compiler\Dependency;
+
+use Lechimp\PHP_JS\Compiler\Dependency\Locator;
+use Lechimp\PHP_JS\Compiler\Dependency\Psr4Locator;
+
+class Psr4LocatorTest extends \PHPUnit\Framework\TestCase {
+    public function setUp() {
+        $this->inner = $this->createMock(Locator::class);
+        $this->psr4_locator= new Psr4Locator(
+            $this->inner,
+            "Lechimp\\PHP_JS\\Test\\Compiler\\Dependency\\",
+            __DIR__
+        );
+    }
+
+    public function test_isInternalDependency_delegates_unknown() {
+        $filename = "FILENAME";
+        $expected = false;
+
+        $this->inner
+            ->expects($this->once())
+            ->method("isInternalDependency")
+            ->with($filename)
+            ->willReturn($expected);
+
+        $results = $this->psr4_locator->isInternalDependency($filename);
+
+        $this->assertEquals($expected, $results);
+    }
+
+    public function test_getFilenameOfDependency_delegates_unknown() {
+        $filename = "FILENAME";
+        $expected = "RESULT";
+
+        $this->inner
+            ->expects($this->once())
+            ->method("getFilenameOfDependency")
+            ->with($filename)
+            ->willReturn($expected);
+
+        $results = $this->psr4_locator->getFilenameOfDependency($filename);
+
+        $this->assertEquals($expected, $results);
+    }
+
+    public function test_isInternalDependency_is_false_for_known_files() {
+        $this->assertEquals(
+            false,
+            $this->psr4_locator->isInternalDependency(self::class)
+        ); 
+    }
+
+    public function test_getFilenameOfDependency_uses_psr4() {
+        $this->assertEquals(
+            __FILE__,
+            $this->psr4_locator->getFilenameOfDependency(self::class)
+        ); 
+    }
+}
