8d1429b8aee52f742af266c5d9d33b6fab46a8c6
Richard Klees <richard.klees@rwth-aachen.de>
2019-03-10 21:27:09 +0100

    added some newlines in TestRunner

--- js_tests/main.php
+++ js_tests/main.php
@@ -2,20 +2,27 @@
 
 use Lechimp\PHP_JS\JS_Tests;
 
-class MyScript implements Lechimp\PHP_JS\JS\Script {
+class TestRunner implements Lechimp\PHP_JS\JS\Script {
     public function execute() {
         $tests = $this->buildTests();
         $result = true;
 
+        echo "\n";
+
         foreach ($tests as $test) {
             $r = $test->perform();
-            echo $test->name().": ".($r ? "ok" : "fail");
+            echo $test->name().": ".($r ? "ok" : "fail")."\n";
             $result = $result && $r;
         }
 
+        echo "\n";
+
         if (!$result) {
+            echo "Some Tests failed.\n";
             exit(1);
         }
+
+        echo "Tests ok!\n";
     }
 
     protected function buildTests() {
