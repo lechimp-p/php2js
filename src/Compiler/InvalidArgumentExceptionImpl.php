b8080271d188f83777e4d2ff382a01dc28728954
Richard Klees <richard.klees@rwth-aachen.de>
2019-04-04 20:30:43 +0200

    added Impl-class for InvalidArgumentException

---
+++ src/Compiler/InvalidArgumentExceptionImpl.php
@@ -0,0 +1,36 @@
+<?php
+/**********************************************************************
+ * php.js runs PHP code on the client side using javascript.
+ * Copyright (C) 2017 Richard Klees <richard.klees@rwth-aachen.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ **********************************************************************/
+
+declare(strict_types=1);
+
+/**
+ * ATTENTION: This is not supposed to work in a PHP-environment.
+ * This is just a stub that gets compiled to JS to implement the
+ * JS\PhpArray. Do not use it yourself.
+ */
+class InvalidArgumentException { 
+    /**
+     * @var string
+     */
+    protected $message;
+
+    public function __construct(string $message) {
+        $this->message = $message;
+    }
+}
